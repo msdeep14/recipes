3c35c14980963a8dbd7ca53cf0295db60bf62a75
Mandeep Singh <singhdth@amazon.com>
2020-05-21 14:04:21 +0530

    Updates

---
+++ precache-manifest.24978761ab156c8d669722a80a3aa4b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d29bc38535c2e1ce64483b448c64069c",
+    "url": "/recipes/index.html"
+  },
+  {
+    "revision": "91c9610f54476c6e76bf",
+    "url": "/recipes/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1ffa67b7d5bcaaf5056b",
+    "url": "/recipes/static/js/2.20879442.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipes/static/js/2.20879442.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91c9610f54476c6e76bf",
+    "url": "/recipes/static/js/main.cb070900.chunk.js"
+  },
+  {
+    "revision": "c8ab3bf18217eb1e5c85",
+    "url": "/recipes/static/js/runtime-main.3b55c3fa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/recipes/static/media/logo.5d5d9eef.svg"
+  }
+]);
